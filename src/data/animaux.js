c26e7757b2be491657da8eb0e6d496de3ac6a223
PierrePenaud <pierrot.penaud@gmail.com>
2026-05-24 15:03:31 +0200

    data: ajout liste des 9 animaux

---
+++ src/data/animaux.js
@@ -0,0 +1,13 @@
+const ANIMAUX = [
+  { id: "chien-vert",   type: "chien",  couleur: "vert",  image: "/src/assets/icons/chien-vert.png"  },
+  { id: "chien-jaune",  type: "chien",  couleur: "jaune", image: "/src/assets/icons/chien-jaune.png" },
+  { id: "chien-rouge",  type: "chien",  couleur: "rouge", image: "/src/assets/icons/chien-rouge.png" },
+  { id: "chat-vert",    type: "chat",   couleur: "vert",  image: "/src/assets/icons/chat-vert.png"   },
+  { id: "chat-jaune",   type: "chat",   couleur: "jaune", image: "/src/assets/icons/chat-jaune.png"  },
+  { id: "chat-rouge",   type: "chat",   couleur: "rouge", image: "/src/assets/icons/chat-rouge.png"  },
+  { id: "oiseau-vert",  type: "oiseau", couleur: "vert",  image: "/src/assets/icons/oiseau-vert.png" },
+  { id: "oiseau-jaune", type: "oiseau", couleur: "jaune", image: "/src/assets/icons/oiseau-jaune.png"},
+  { id: "oiseau-rouge", type: "oiseau", couleur: "rouge", image: "/src/assets/icons/oiseau-rouge.png"},
+]
+
+export default ANIMAUX
