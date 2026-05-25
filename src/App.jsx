92496be88d97585aa9acf7c76ad15a28858898f3
PierrePenaud <pierrot.penaud@gmail.com>
2026-05-25 22:11:20 +0200

    ajout grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import ANIMAUX from './data/animaux'
 
 function App() {
   return (
-    <div className="p-8 flex flex-col gap-8">
+    <div className="p-8 grid grid-cols-3 gap-8">
 
-      <GridCell animal={null}/>
+      {Array(9).fill(null).map((_, index) => (
+        <GridCell key={index} animal={null} />
+      ))}
       
-      <GridCell animal={ANIMAUX[0]}/>
 
     </div>
   )
