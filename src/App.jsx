ee23b6c784d1b805ec757f6ae66c93848ed6f1ef
PierrePenaud <pierrot.penaud@gmail.com>
2026-05-30 22:34:24 +0200

    feat: drag & drop complet dans les deux sens

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
-import Grid from "./components/Grid"
-import ANIMAUX from "./data/animaux"
-import Reserve from "./components/Reserve"
-import { useState } from "react"
-import { DndContext } from "@dnd-kit/core"
-
+import Grid from "./components/Grid";
+import ANIMAUX from "./data/animaux";
+import Reserve from "./components/Reserve";
+import { act, useState } from "react";
+import { DndContext, DragOverlay } from "@dnd-kit/core";
+import TileAnimal from "./components/TileAnimal";
 
 function App() {
-  const [grille, setGrille] = useState(Array(9).fill(null))
-  const animauxDisponibles = ANIMAUX.filter(animal => !grille.includes(animal.id))
+  const [grille, setGrille] = useState(Array(9).fill(null));
+  const [animalActif, setAnimalActif] = useState(null);
+  const animauxDisponibles = ANIMAUX.filter(
+    (animal) => !grille.includes(animal.id) && animal.id !== animalActif?.id,
+  );
+
+  function handleDragStart(event) {
+    console.log("drag start", event);
+    const animal = ANIMAUX.find((a) => a.id === event.active.id);
+    const { active, over } = event
+    const indexOrigine = grille.indexOf(active.id);
+    const nouvelleGrille = [...grille];
+    if (indexOrigine !== -1) nouvelleGrille[indexOrigine] = null;
+    setGrille(nouvelleGrille);
+    setAnimalActif(animal);
+  }
 
   function handleDragEnd(event) {
-    console.log("drag end", event)
-    const { active, over } = event
-    if (!over) return  // ← ajoute cette ligne
-    const nouvelleGrille = [...grille]
-    nouvelleGrille[over.id] = active.id
-    setGrille(nouvelleGrille)
+    console.log("drag end", event);
+    const { active, over } = event;
+    const indexOrigine = grille.indexOf(active.id);
+    if (!over) return setAnimalActif(null);
+    const nouvelleGrille = [...grille];
+    if (indexOrigine !== -1) nouvelleGrille[indexOrigine] = null
+    nouvelleGrille[over.id] = active.id;
+    setGrille(nouvelleGrille);
+    setAnimalActif(null);
   }
 
   return (
     <div className="p-8 flex flex-col gap-8">
-      <DndContext onDragEnd={handleDragEnd}>
-        <Grid grille={grille}/>
-        <Reserve animaux={animauxDisponibles}/>
+      <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+        <DragOverlay>
+          {animalActif ? <TileAnimal animal={animalActif} /> : null}
+        </DragOverlay>
+        <Grid grille={grille} />
+        <Reserve animaux={animauxDisponibles} />
       </DndContext>
-      
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
