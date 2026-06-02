414eccb9f223faf827df3646733e35ee613dc6e2
PierrePenaud <pierrot.penaud@gmail.com>
2026-06-02 22:00:13 +0200

    data: ajout niveaux de difficulté dans les fiches

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -6,10 +6,11 @@ function LoginScreen({ onLogin }) {
         <div>
             <div></div>
             <input
+                className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 value={prenom}
                 onChange={e => setPrenom(e.target.value)}
             />
-            <button
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 onClick={() => onLogin(prenom)}
             >
                 Valider
